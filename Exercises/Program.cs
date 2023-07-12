c1eace7ded673e7f652089c3eb0aeab71fbb35ae
PrashastVats1 <prashastvats@gmail.com>
2023-07-12 15:22:55 +0530

    second exercise commit

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -18,6 +18,8 @@ namespace Exercises
                 int tryParsedInt;
                 int.TryParse(input, out tryParsedInt);
                 Console.WriteLine("TryParsed number is:\t" + tryParsedInt);
+                Console.WriteLine("TryParsed number multiplied by 5 is:\t" 
+                    + tryParsedInt * 5);
             }
             catch (Exception ex) { Console.WriteLine("Error!! " + ex.Message); }
             finally { Console.WriteLine("\n"); ; }
@@ -29,6 +31,8 @@ namespace Exercises
                 input1 = Console.ReadLine();
                 double convertedNumber = Convert.ToDouble(input1);
                 Console.WriteLine("Convert number is:\t" + convertedNumber);
+                Console.WriteLine("Convert number multiplied by 5 is:\t" +
+                    convertedNumber * 5);
             }
             catch (Exception ex) { Console.WriteLine("Error!! " + ex.Message); }
             finally { Console.WriteLine("\n"); }
@@ -40,6 +44,35 @@ namespace Exercises
                 input2 = Console.ReadLine();
                 decimal parsedNumber = decimal.Parse(input2);
                 Console.WriteLine("Parsed number is:\t" + parsedNumber);
+                Console.WriteLine("Parsed number multiplied by 5 is:\t" +
+                    parsedNumber * 5);
+            }
+            catch (Exception ex) { Console.WriteLine("Error!! " + ex.Message); }
+            finally { Console.WriteLine("\n"); }
+
+            try
+            {
+                string input2;
+                Console.WriteLine("Enter date");
+                input2 = Console.ReadLine();
+                DateTime convertedDate = Convert.ToDateTime(input2);
+                Console.WriteLine("Converted Date is:\t" + convertedDate);
+                DateTime lastMonth = convertedDate.AddMonths(-1);
+                Console.WriteLine("Converted month after reducing a month is:\t" +
+                    lastMonth);
+            }
+            catch (Exception ex) { Console.WriteLine("Error!! " + ex.Message); }
+            finally { Console.WriteLine("\n"); }
+
+            try
+            {
+                Console.WriteLine("Input Time in hh:mm:ss \n");
+                string timeString = Console.ReadLine();
+                TimeSpan timeSpan = TimeSpan.Parse(timeString);
+                Console.WriteLine("Converted TimeSpan is "+timeSpan);
+                TimeSpan past2hours = new TimeSpan(2, 0, 0);
+                Console.WriteLine("Converted time after reducing 2 hours is:\t" +
+                    (timeSpan-past2hours));
             }
             catch (Exception ex) { Console.WriteLine("Error!! " + ex.Message); }
             finally { Console.ReadKey(); }
